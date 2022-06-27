847f79e15c4d3a3b7d799e85923cb018fea7eb3b
younjehanno <youn.jehanno@hupi.fr>
2022-06-27 15:55:35 +0200

    Add project code and documentation

---
+++ calculator/calculations.py
@@ -0,0 +1,57 @@
+# calculator/calculations.py
+
+"""Provide several sample math calculations.
+
+This module allows the user to make mathematical calculations.
+
+Examples:
+    >>> from calculator import calculations
+    >>> calculations.add(2, 4)
+    6.0
+    >>> calculations.multiply(2.0, 4.0)
+    8.0
+    >>> from calculator.calculations import divide
+    >>> divide(4.0, 2)
+    2.0
+
+The module contains the following functions:
+
+- `add(a, b)` - Returns the sum of two numbers.
+- `subtract(a, b)` - Returns the difference of two numbers.
+- `multiply(a, b)` - Returns the product of two numbers.
+- `divide(a, b)` - Returns the quotient of two numbers.
+"""
+
+from typing import Union
+
+def add(a: Union[float, int], b: Union[float, int]) -> float:
+    """Compute and return the sum of two numbers.
+
+    Examples:
+        >>> add(4.0, 2.0)
+        6.0
+        >>> add(4, 2)
+        6.0
+
+    Args:
+        a: A number representing the first addend in the addition.
+        b: A number representing the second addend in the addition.
+
+    Returns:
+        A number representing the artihmetic sum of `a` and `b`.
+    """
+    return float(a + b)
+
+# help(add)
+# print(add.__doc__)
+
+def subtract(a, b):
+    return float(a - b)
+
+def multiply(a, b):
+    return float(a * b)
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("division by zero")
+    return float(a / b)
